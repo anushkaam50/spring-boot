6a74b968513842276cb38e40cc51064ac6f64aa6
anushkaam50 <anushkaam50@gmail.com>
2025-09-22 22:55:14 +0530

    Scope - Prototype

--- src/main/java/com/w1/_springboot_intro/sbintro/AppConfig.java
+++ src/main/java/com/w1/_springboot_intro/sbintro/AppConfig.java
@@ -2,6 +2,7 @@ package com.w1._springboot_intro.sbintro;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 
 @Configuration // this class contains methods to define and configure beans
@@ -9,6 +10,8 @@ public class AppConfig {
 
 //    eatApple is factory method
     @Bean
+//    @Scope("singleton") - default scope is always singleton; for n of objects, generated hashcode is same for Apple Bean
+    @Scope("prototype") // hashcode will be different for each this Apple Bean
      Apple eatApple()    {             // creates bean
      return new Apple();}               // registers bean
 }
--- src/main/java/com/w1/_springboot_intro/sbintro/SbintroApplication.java
+++ src/main/java/com/w1/_springboot_intro/sbintro/SbintroApplication.java
@@ -9,7 +9,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SbintroApplication implements CommandLineRunner {
 //    autowired - object of apple should inject in non-static main; injected apple dependency in SbintroApplication bean
     @Autowired
-     Apple obj;
+     Apple a1;
+
+    @Autowired
+    Apple a2;
 
 //  static main
 	public static void main(String[] args) {
@@ -19,6 +22,19 @@ public class SbintroApplication implements CommandLineRunner {
 //    non-static main
     @Override
     public void run(String... args) throws Exception {
-        obj.eatApple();
+        a1.eatApple();
+        a2.eatApple();
+
+        System.out.println(a1.hashCode() == a2.hashCode() ? "true" : "false");
+
+
+//    2025-09-22T22:52:41.617+05:30  INFO 14791 --- [sbintro] [           main] w.s.c.ServletWebServerApplicationContext : Root WebApplicationContext: initialization completed in 1294 ms
+//    PostConstruct - This is initializing the bean before using it
+//    PostConstruct - This is initializing the bean before using it
+//2025-09-22T22:52:42.105+05:30  INFO 14791 --- [sbintro] [           main] o.s.b.w.embedded.tomcat.TomcatWebServer  : Tomcat started on port 8080 (http) with context path '/'
+//            2025-09-22T22:52:42.116+05:30  INFO 14791 --- [sbintro] [           main] c.w._.sbintro.SbintroApplication         : Started SbintroApplication in 2.605 seconds (process running for 3.142)
+//    Bean is used thr' psvm call - Im eating
+//    Bean is used thr' psvm call - Im eating
+//            false
     }
 }
